2e17e3e5345fddb28f2cd898691b312710a382be
Timothy Capper <timcapper@gmail.com>
2025-01-14 18:04:41 +0000

    Deleted files related to projects, renamed blog

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -38,7 +38,7 @@ export type SiteConfig = {
 };
 
 const siteConfig: SiteConfig = {
-    title: 'Dante',
+    title: 'Infrastructure is hard',
     subtitle: 'Minimal Astro.js theme',
     description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
     image: {
